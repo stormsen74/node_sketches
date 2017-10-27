59174cdece2b6aba9e2edd9d361f4c19bd4b0255
stormsen <micha@edit23.de>
2017-10-27 19:27:26 +0200

    ... local commit (check)

--- source/Sketch_8.js
+++ source/Sketch_8.js
@@ -33,12 +33,17 @@ class Sketch_8 extends SketchTemplate {
          --------------------------------------------*/
 
         this.sketch.CONFIG = {
-            BASE: 5,
-            NUM_POINTS: 360,
-            R: 12,
-            POINT_SCALE: 1,
+            ATTRACTOR: {
+                a: Math.random() * 4 - 2,
+                b: Math.random() * 4 - 2,
+                c: Math.random() * 4 - 2,
+                d: Math.random() * 4 - 2
+            },
+            DRAWS_PER_CALL: 1000,
             METHODS: {
-                save: function () {
+                shuffle: function () {
+                },
+                reset: function () {
                 }
             }
         };
@@ -64,11 +69,27 @@ class Sketch_8 extends SketchTemplate {
 
         this.sketch.getValue = function (x, y) {
             // return (x + y) * 0.001 * Math.PI * 2;
-            return (Math.sin(x * 0.01) + Math.sin(y * 0.01)) * Math.PI * 2;
+
+            // return (Math.sin(x * 0.01) + Math.sin(y * 0.01)) * Math.PI * 2;
+
+            // clifford attractor
+            // http://paulbourke.net/fractals/clifford/
+
+            // scale down x and y
+            let scale = 0.005;
+            x = (x - this.width * .5) * scale;
+            y = (y - this.height * .5) * scale;
+
+            // attactor gives new x, y for old one.
+            let x1 = Math.sin(this.CONFIG.ATTRACTOR.a * y) + this.CONFIG.ATTRACTOR.c * Math.cos(this.CONFIG.ATTRACTOR.a * x);
+            let y1 = Math.sin(this.CONFIG.ATTRACTOR.b * x) + this.CONFIG.ATTRACTOR.d * Math.cos(this.CONFIG.ATTRACTOR.b * y);
+
+            // find angle from old to new. that's the value.
+            return Math.atan2(y1 - y, x1 - x);
         };
 
         this.sketch.render = function (value) {
-            this.strokeStyle = chromatism.convert(({h: Math.abs(value) * 5, s: 50, l: 40})).hex;
+            this.strokeStyle = chromatism.convert(({h: 20 + Math.abs(value) * 20, s: 50, l: 40})).hex;
             this.rotate(value);
             this.beginPath();
             this.moveTo(0, 0);
@@ -79,17 +100,17 @@ class Sketch_8 extends SketchTemplate {
         this.sketch.plotField = function () {
             console.log('plotField');
 
-            this.globalCompositeOperation = 'overlay';
+            // this.globalCompositeOperation = 'overlay';
             // let color = ({h: 90, s: 50, l: 40});
             // this.strokeStyle = chromatism.convert(color).hex;
-            this.lineWidth = 1;
+            // this.strokeStyle = '#ffffff';
+            this.lineWidth = .5;
 
-            for (let i = 0; i < 3000; i++) {
+            for (let i = 0; i < this.CONFIG.DRAWS_PER_CALL; i++) {
                 let x = Math.random() * this.width;
                 let y = Math.random() * this.height;
 
                 let value = this.getValue(x, y);
-                console.log(value)
 
                 this.save();
 
@@ -122,14 +143,35 @@ class Sketch_8 extends SketchTemplate {
 
         //this.gui.add(this.sketch.CONFIG, 'BASE').min(1).max(12).step(1).name('BASE').onChange(this.updateParams.bind(this));
 
-        this.gui.add(this.sketch.CONFIG, 'BASE').min(1).max(12).step(1).name('BASE');
-        this.gui.add(this.sketch.CONFIG, 'NUM_POINTS').min(0).max(720).step(1).name('NUM_POINTS');
-        this.gui.add(this.sketch.CONFIG, 'R').min(0).max(36).step(.1).name('R');
-        this.gui.add(this.sketch.CONFIG, 'POINT_SCALE').min(0).max(2).step(.01).name('POINT_SCALE');
+        this.gui.add(this.sketch.CONFIG, 'DRAWS_PER_CALL').min(100).max(20000).step(1).name('DRAWS_PER_CALL');
+        this.gui.add(this.sketch.CONFIG.METHODS, 'reset').onChange(this.reset.bind(this));
+
+        let f1 = this.gui.addFolder('clifford attractor');
+        f1.add(this.sketch.CONFIG.ATTRACTOR, 'a').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
+        f1.add(this.sketch.CONFIG.ATTRACTOR, 'b').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
+        f1.add(this.sketch.CONFIG.ATTRACTOR, 'c').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
+        f1.add(this.sketch.CONFIG.ATTRACTOR, 'd').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
+        f1.add(this.sketch.CONFIG.METHODS, 'shuffle').onChange(this.shuffleParameters.bind(this));
+        f1.open();
+
     }
 
     updateParams() {
+        this.sketch.clear();
+        this.sketch.plotField();
+    }
 
+    shuffleParameters() {
+        this.sketch.clear();
+        this.sketch.CONFIG.ATTRACTOR.a = Math.random() * 4 - 2;
+        this.sketch.CONFIG.ATTRACTOR.b = Math.random() * 4 - 2;
+        this.sketch.CONFIG.ATTRACTOR.c = Math.random() * 4 - 2;
+        this.sketch.CONFIG.ATTRACTOR.d = Math.random() * 4 - 2;
+        this.sketch.plotField();
+    }
+
+    reset() {
+        this.sketch.clear();
     }
 
 
