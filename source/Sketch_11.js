c806e75117f36b7d39a4ec102ad695351ad4b995
Michael Sturm <micha@edit23.de>
2018-01-11 17:25:58 +0100

    ... added more params

--- source/Sketch_11.js
+++ source/Sketch_11.js
@@ -30,12 +30,18 @@ class Sketch_11 extends SketchTemplate {
 
         this.sketch.CONFIG = {
             NOISE_SCALE: 0.0015,
+            EPSILON: 0.0001,
             STRENGTH: 1,
+            NOISE_3D: false,
+            dt_noise_z: .002,
+            FADE_ALPHA: .07,
             LIFETIME: {
                 min: 20,
                 max: 50
             },
             METHODS: {
+                run: function () {
+                },
                 save: function () {
                 }
             }
@@ -56,9 +62,7 @@ class Sketch_11 extends SketchTemplate {
         // this.sketch.nParticles = 1;
         this.sketch.vMouse = new Vector2();
 
-
         this.sketch.colorMap = chromatism.fade(10, '#ddb321', '#21d6dd').hex;
-        console.log(this.sketch.colorMap)
 
 
         /*--------------------------------------------
@@ -91,16 +95,16 @@ class Sketch_11 extends SketchTemplate {
 
 
         this.sketch.getNoise = function (x, y) {
-            // return this.noise.noise2D(x, y);
-            return this.noise.noise3D(x, y, this.noise_z);
+            return !this.CONFIG.NOISE_3D ? this.noise.noise2D(x, y) : this.noise.noise3D(x, y, this.noise_z);
+            // return this.noise.noise3D(x, y, this.noise_z);
         };
 
 
         this.sketch.curlNoise = function (pos) {
-            let dx = this.getNoise(pos.x + this.epsilon, pos.y) - this.getNoise(pos.x - this.epsilon, pos.y);
-            let dy = this.getNoise(pos.x, pos.y + this.epsilon) - this.getNoise(pos.x, pos.y - this.epsilon);
+            let dx = this.getNoise(pos.x + this.CONFIG.EPSILON, pos.y) - this.getNoise(pos.x - this.CONFIG.EPSILON, pos.y);
+            let dy = this.getNoise(pos.x, pos.y + this.CONFIG.EPSILON) - this.getNoise(pos.x, pos.y - this.CONFIG.EPSILON);
             let v = new Vector2(dy, -dx);
-            return v.multiplyScalar(this.CONFIG.STRENGTH / this.epsilon);
+            return v.multiplyScalar(this.CONFIG.STRENGTH / this.CONFIG.EPSILON);
         };
 
         this.sketch.mousedown = function () {
@@ -113,6 +117,7 @@ class Sketch_11 extends SketchTemplate {
 
         this.sketch.draw = function () {
 
+            this.fillStyle = "rgba(5,5,5," + this.CONFIG.FADE_ALPHA + ")";
             this.fillRect(0, 0, this.width, this.height);
 
             for (let i = 0; i < this.nParticles; i++) {
@@ -120,10 +125,7 @@ class Sketch_11 extends SketchTemplate {
                 let pos = p.clone().multiplyScalar(this.CONFIG.NOISE_SCALE);
                 let dir = this.curlNoise(pos);
 
-                // console.log(parseInt(MathUtils.convertToRange(dir.length(), [0, 10], [0, this.colorMap.length])));
                 this.strokeStyle = this.colorMap[parseInt(MathUtils.convertToRange(dir.length(), [0, 15], [0, this.colorMap.length]))];
-
-                // this.save();
 
                 this.beginPath();
                 this.moveTo(p.x, p.y);
@@ -131,13 +133,11 @@ class Sketch_11 extends SketchTemplate {
                 this.lineTo(p.x, p.y);
                 this.stroke();
 
-                // this.restore();
-
                 if (p.x < 0 || p.x > this.width || p.y < 0 || p.y > this.height || --p.t <= 0) this.reset(p);
             }
 
 
-            this.noise_z += .002;
+            this.noise_z += this.CONFIG.dt_noise_z;
 
         };
 
@@ -160,12 +160,27 @@ class Sketch_11 extends SketchTemplate {
         document.getElementById('dat-container').appendChild(this.gui.domElement);
 
         this.gui.add(this.sketch.CONFIG, 'NOISE_SCALE').min(0.0005).max(0.01).step(.0001).name('NOISE_SCALE');
+        this.gui.add(this.sketch.CONFIG, 'EPSILON').min(0.00001).max(1).step(.00001).name('EPSILON');
         this.gui.add(this.sketch.CONFIG, 'STRENGTH').min(0).max(5).step(.01).name('STRENGTH');
+        this.gui.add(this.sketch.CONFIG, 'NOISE_3D').name('NOISE_3D');
+        this.gui.add(this.sketch.CONFIG, 'dt_noise_z').min(0).max(.01).step(.001).name('dt_noise_z');
+
+        let stroke_options = this.gui.addFolder('stroke');
+        stroke_options.add(this.sketch.CONFIG.LIFETIME, 'min').min(0).max(100).step(1);
+        stroke_options.add(this.sketch.CONFIG.LIFETIME, 'max').min(0).max(200).step(1);
+        stroke_options.add(this.sketch.CONFIG, 'FADE_ALPHA').min(.01).max(.5).step(.01).name('FADE_ALPHA');
+
         this.gui.add(this.sketch.CONFIG.METHODS, 'save').onChange(this.save.bind(this));
+        this.gui.add(this.sketch.CONFIG.METHODS, 'run').onChange(this.run.bind(this));
     }
 
     updateParams() {
 
+    }
+
+
+    run() {
+        this.sketch.running = !this.sketch.running;
     }
 
     save() {
