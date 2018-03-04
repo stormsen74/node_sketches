f3664d88cca1bfedc26327c99a069e638916840b
stormsen <micha@edit23.de>
2018-03-04 22:29:09 +0100

    added sketch 12 -> electrical field

--- source/sketch_12.js
+++ source/sketch_12.js
@@ -1,3 +1,5 @@
+import Draggable from "gsap/Draggable";
+
 /**
  * Created by STORMSEN on 14.06.2017.
  */
@@ -20,13 +22,7 @@ class Sketch_12 extends SketchTemplate {
     constructor() {
         super();
 
-        console.log('Sketch_8!');
-
-        // https://gka.github.io/chroma.js/
-        let c = chroma.hsl(330, 1, 0.6);
-        c = chroma.hsl(333.0, 1, 0.6);
-        console.log(c.hex())
-
+        console.log('Sketch_12!');
 
         /*--------------------------------------------
          ~ sketch variables
@@ -37,14 +33,50 @@ class Sketch_12 extends SketchTemplate {
         this.sketch.vCenter = new Vector2(this.sketch.width * .5, this.sketch.height * .5);
         this.sketch.points = [];
 
-        this.sketch.forceStep = 0;
-        let Q1 = new Vector2(200, this.sketch.height * .5);
+        let Q1 = new Vector2(200, this.sketch.height * .2);
         let Q2 = new Vector2(this.sketch.width - 200, this.sketch.height * .5);
 
         Q1.charge = 1.5;
         Q2.charge = -1.5;
 
         this.sketch.fields = [Q1, Q2];
+        this.sketch.plotPoints = [];
+
+        /*--------------------------------------------
+         ~ dom-stuff
+         --------------------------------------------*/
+
+        let screen = document.getElementById('screen');
+
+        this.sketch.pStart = document.createElement('div');
+        this.sketch.pStart.classList.add('pStart');
+        this.sketch.pStart.id = 'pStart';
+        screen.appendChild(this.sketch.pStart);
+        this.sketch._pstart = Draggable.create(this.sketch.pStart, {
+            type: "x,y", edgeResistance: 0.5, bounds: "#screen", throwProps: false,
+            onDrag: this.onDrag.bind(this)
+        });
+
+
+        this.sketch.pEnd = document.createElement('div');
+        this.sketch.pEnd.classList.add('pEnd');
+        this.sketch.pEnd.id = 'pEnd';
+        screen.appendChild(this.sketch.pEnd);
+
+        this.sketch._pend = Draggable.create(this.sketch.pEnd, {
+            type: "x,y", edgeResistance: 0.5, bounds: "#screen", throwProps: false,
+            onDrag: this.onDrag.bind(this),
+            onDragStart: this.onDragStart.bind(this),
+            onDragEnd: this.onDragEnd.bind(this),
+        });
+
+        TweenLite.set("#pStart", {x: this.sketch.fields[0].x, y: this.sketch.fields[0].y});
+        TweenLite.set("#pEnd", {x: this.sketch.fields[1].x, y: this.sketch.fields[1].y});
+
+        this.sketch._pstart[0].x = this.sketch.fields[0].x;
+        this.sketch._pstart[0].y = this.sketch.fields[0].y;
+        this.sketch._pend[0].x = this.sketch.fields[1].x;
+        this.sketch._pend[0].y = this.sketch.fields[1].y;
 
 
         /*--------------------------------------------
@@ -52,18 +84,18 @@ class Sketch_12 extends SketchTemplate {
          --------------------------------------------*/
 
         this.sketch.CONFIG = {
-            ATTRACTOR: {
-                a: Math.random() * 4 - 2,
-                b: Math.random() * 4 - 2,
-                c: Math.random() * 4 - 2,
-                d: Math.random() * 4 - 2
+            NUM_RENDER_STEPS: 1,
+            OPTIONS: {
+                opacity: .2,
+                overdraw: false
             },
-            DRAWS_PER_CALL: 100,
-            NUM_RENDER_STEPS: 3,
+            POINTS: {
+                maxPoints: 100
+            },
             METHODS: {
                 shuffle: function () {
                 },
-                run: function () {
+                toggleRun: function () {
                 },
                 renderField: function () {
                 },
@@ -82,64 +114,28 @@ class Sketch_12 extends SketchTemplate {
         this.sketch.setup = function () {
             console.log('setup');
 
+
             this.lineWidth = .5;
             this.strokeStyle = 'rgba(196, 174, 69, .2)';
             this.fillStyle = "rgba(23, 23, 26, 1)";
-            this.globalCompositeOperation = 'lighter';
+            // this.globalCompositeOperation = 'lighter';
+            this.autoclear = false;
 
-
-            for (let y = 0; y < this.height; y += 10) {
-                this.points.push({
-                    x: 0,
-                    y: y,
-                    vx: 0,
-                    vy: 0
-                })
-            }
-
-        };
-
-        this.sketch.mousedown = function () {
-            // this.plotField();
-            // this.plotForce();
-
-
-            // Q2.set(this.mouse.x, this.mouse.y)
-            // this.steppedRender();
-
-        };
-
-
-        this.sketch.steppedRender = function () {
-
-            this.clear();
-            this.fillRect(0, 0, this.width, this.height);
-
-            this.forceStep = 0;
-            while (this.forceStep <= this.CONFIG.NUM_RENDER_STEPS) {
-                this.filter = 'blur(' + mathUtils.convertToRange(this.forceStep, [0, this.CONFIG.NUM_RENDER_STEPS], [10, 0]) + 'px)';
-                this.forceStep += 1;
-                this.plotForce();
-            }
-
-
-        };
-
-        this.sketch.mousemove = function () {
         };
 
 
         this.sketch.getForce = function (vLocation) {
 
             let eVecRes = new Vector2();
+            let _field = new Vector2();
 
             for (var i = 0; i < this.fields.length; i++) {
-                let field = this.fields[i];
-                field.rVec = Vector2.subtract(vLocation, field);
-                field.eVec = field.rVec.clone();
-                field.eVec.multiplyScalar(field.charge / (4 * Math.PI));
-                field.eVec.multiplyScalar(1 / Math.pow(field.rVec.length(), 2));
-                field.eVec.multiplyScalar(100000);
+                _field = this.fields[i];
+                _field.rVec = Vector2.subtract(vLocation, _field);
+                _field.eVec = _field.rVec.clone();
+                _field.eVec.multiplyScalar(_field.charge / (4 * Math.PI));
+                _field.eVec.multiplyScalar(1 / Math.pow(_field.rVec.length(), 2));
+                _field.eVec.multiplyScalar(100000);
 
                 i == 0 ? eVecRes = this.fields[0].eVec : eVecRes.add(this.fields[i].eVec);
             }
@@ -148,128 +144,41 @@ class Sketch_12 extends SketchTemplate {
 
         };
 
-
-        this.sketch.getValue = function (x, y) {
-
-            // https://www.bit-101.com/blog/2017/10/23/flow-fields-part-i/
-
-
-            // return (y - x) * 0.001 * Math.PI * 2;
-
-            // return (Math.sin(x * 0.01) + Math.sin(y * 0.01)) * Math.PI * 2;
-
-            // return
-
-            // let scale = 0.01;
-            // x = (x - this.width * .5) * scale;
-            // y = (y - this.height * .5) * scale;
-            // // let v = new Vector2(Math.sin(y), Math.sin(x));
-            // let v = new Vector2(Math.pow(y, 2), Math.pow(x, 2));
-            // let v = new Vector2(y, -x);
-            
-            // return v.angle();
-
-            // return
-
-
-            // clifford attractor
-            // http://paulbourke.net/fractals/clifford/
-
-            // scale down x and y
-
-            let scale = 0.005;
-            x = (x - this.width * .5) * scale;
-            y = (y - this.height * .5) * scale;
-
-            // attactor gives new x, y for old one.
-            let x1 = Math.sin(this.CONFIG.ATTRACTOR.a * y) + this.CONFIG.ATTRACTOR.c * Math.cos(this.CONFIG.ATTRACTOR.a * x);
-            let y1 = Math.sin(this.CONFIG.ATTRACTOR.b * x) + this.CONFIG.ATTRACTOR.d * Math.cos(this.CONFIG.ATTRACTOR.b * y);
-
-            // find angle from old to new. that's the value.
-            return Math.atan2(y1 - y, x1 - x);
-        };
-
-
-        this.sketch.render = function () {
-
-            for (var i = 0; i < this.points.length; i++) {
-                // get each point and do what we did before with a single point
-                let p = this.points[i];
-                let value = this.getValue(p.x, p.y);
-                p.vx += Math.cos(value) * 0.2;
-                p.vy += Math.sin(value) * 0.2;
-
-                // move to current position
-                this.beginPath();
-                this.moveTo(p.x, p.y);
-
-                // add velocity to position and line to new position
-                p.x += p.vx;
-                p.y += p.vy;
-                this.lineTo(p.x, p.y);
-                this.stroke();
-
-                // apply some friction so point doesn't speed up too much
-                p.vx *= 0.99;
-                p.vy *= 0.99;
-
-                // wrap around edges of screen
-                if (p.x > this.width) p.x = 0;
-                if (p.y > this.height) p.y = 0;
-                if (p.x < 0) p.x = this.width;
-                if (p.y < 0) p.y = this.height;
-            }
-        };
-
-        this.sketch.update = function () {
-            this.render();
-        };
-
-
         this.sketch.renderForce = function (vForce) {
 
-            let mag = Math.min(vForce.length(), 200);
-            let invertMag = (200 - mag) * .7;
+            let maxLength = 75;
+
+            let force = vForce.clone();
+            if (force.length() > maxLength) force.setLength(maxLength);
 
             this.strokeStyle = chroma.hsl(
-                mathUtils.convertToRange(mag, [0, 200], [150, 260]),
+                mathUtils.convertToRange(force.length(), [0, maxLength], [150, 260]),
                 .5,
-                .4,
+                mathUtils.convertToRange(force.length(), [0, maxLength], [.2, .6]),
             ).hex();
 
-            this.rotate(vForce.angle());
             this.beginPath();
             this.moveTo(0, 0);
-            this.lineTo(invertMag * .5, -invertMag * .5);
-            // this.lineTo(20, 1);
+            this.lineTo(force.x, force.y);
             this.stroke();
+
         }
-
-
-        this.sketch.renderField = function (value) {
-
-            // this.strokeStyle = chromatism.convert(({h: 20 + Math.abs(value) * 20, s: 50, l: 40})).hex;
-            this.strokeStyle = chroma.hsl(
-                // mathUtils.convertToRange(Math.abs(value), [0, Math.PI * 2], [0, 60]),
-                20 + Math.abs(value) * 20,
-                .5,
-                .4,
-            ).hex();
-
-            this.rotate(value);
-            this.beginPath();
-            this.moveTo(0, 0);
-            // this.lineTo(value * 10, -value * 10);
-            this.lineTo(20, 1);
-            this.stroke();
-        };
-
 
         this.sketch.plotForce = function () {
 
-            for (let i = 0; i < this.CONFIG.DRAWS_PER_CALL; i++) {
-                let x = Math.random() * this.width;
-                let y = Math.random() * this.height;
+            if(this.CONFIG.OPTIONS.overdraw) {
+                this.fillStyle = "rgba(5,5,5," + this.CONFIG.OPTIONS.opacity + ")";
+                this.fillRect(0, 0, this.width, this.height);
+            } else {
+                this.clear();
+                this.fillStyle = "rgba(23, 23, 26, 1)";
+            }
+
+
+            let x, y = 0;
+            for (let i = 0; i < this.CONFIG.POINTS.maxPoints; i++) {
+                x = Math.random() * this.width;
+                y = Math.random() * this.height;
                 let vForce = this.getForce(new Vector2(x, y));
                 this.save();
                 this.translate(x, y);
@@ -278,34 +187,53 @@ class Sketch_12 extends SketchTemplate {
             }
         };
 
-        this.sketch.plotField = function () {
-            // console.log('plotField');
 
-            for (let i = 0; i < this.CONFIG.DRAWS_PER_CALL; i++) {
-                let x = Math.random() * this.width;
-                let y = Math.random() * this.height;
+        this.sketch.mousedown = function () {
+            this.plotForce();
 
-
-                let value = this.getValue(x, y);
-
-                this.save();
-
-                this.translate(x, y);
-
-                this.renderField(value);
-
-                this.restore();
-
-                // this.plotPoint(x, y, 2, 0, '#ff0000', '#cccccc');
-            }
+            // Q2.set(this.mouse.x, this.mouse.y)
 
         };
+
+        this.sketch.mousemove = function () {
+        };
+
+
+        this.sketch.render = function () {
+            // this.clear();
+        };
+
+        this.sketch.update = function () {
+            this.plotForce();
+        };
+
 
     }
 
     /*--------------------------------------------
      ~ class methods
      --------------------------------------------*/
+
+    onDrag() {
+        //console.log(this.sketch._pstart[0].x)
+        this.sketch.fields[0].x = this.sketch._pstart[0].x;
+        this.sketch.fields[0].y = this.sketch._pstart[0].y;
+
+        this.sketch.fields[1].x = this.sketch._pend[0].x;
+        this.sketch.fields[1].y = this.sketch._pend[0].y;
+
+
+        this.plotField();
+        // this.sketch.updateDrag();
+    }
+
+    onDragStart() {
+    };
+
+
+    onDragEnd() {
+        //this.sketch.updateDraw();
+    };
 
     kill() {
         document.getElementById('dat-container').removeChild(this.gui.domElement);
@@ -320,27 +248,17 @@ class Sketch_12 extends SketchTemplate {
         document.getElementById('dat-container').appendChild(this.gui.domElement);
 
 
-        this.gui.add(this.sketch.CONFIG, 'DRAWS_PER_CALL').min(100).max(20000).step(1).name('DRAWS_PER_CALL');
-
-        let f_force = this.gui.addFolder('force');
-        f_force.add(this.sketch.CONFIG, 'NUM_RENDER_STEPS').min(1).max(15).step(1).name('NUM_RENDER_STEPS');
-        f_force.add(this.sketch.CONFIG.METHODS, 'renderForce').onChange(this.renderForce.bind(this));
-        f_force.open();
-
-        let f_field = this.gui.addFolder('field');
-        f_field.add(this.sketch.CONFIG.METHODS, 'run').onChange(this.run.bind(this));
-        f_field.add(this.sketch.CONFIG.METHODS, 'renderField').onChange(this.renderField.bind(this));
-        f_field.open();
-
-        let f_attractor = this.gui.addFolder('clifford attractor');
-        f_attractor.add(this.sketch.CONFIG.ATTRACTOR, 'a').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
-        f_attractor.add(this.sketch.CONFIG.ATTRACTOR, 'b').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
-        f_attractor.add(this.sketch.CONFIG.ATTRACTOR, 'c').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
-        f_attractor.add(this.sketch.CONFIG.ATTRACTOR, 'd').min(-2).max(2).step(.01).onChange(this.updateParams.bind(this)).listen();
-        f_attractor.add(this.sketch.CONFIG.METHODS, 'shuffle').onChange(this.shuffleParameters.bind(this));
-        f_attractor.open();
-
+        this.gui.add(this.sketch.CONFIG.METHODS, 'toggleRun').onChange(this.toggleRun.bind(this));
         this.gui.add(this.sketch.CONFIG.METHODS, 'reset').onChange(this.reset.bind(this));
+
+        let points = this.gui.addFolder('points');
+        points.add(this.sketch.CONFIG.POINTS, 'maxPoints').min(10).max(1000).step(1).name('maxPoints');
+        // points.add(this.sketch.CONFIG.METHODS, 'renderForce').onChange(this.renderForce.bind(this));
+        points.open();
+
+        let f_options = this.gui.addFolder('options');
+        f_options.add(this.sketch.CONFIG.OPTIONS, 'opacity').min(0.001).max(1).step(.001);
+        f_options.add(this.sketch.CONFIG.OPTIONS, 'overdraw').name('overdraw');
     }
 
     updateParams() {
@@ -348,41 +266,20 @@ class Sketch_12 extends SketchTemplate {
         this.sketch.plotField();
     }
 
-    shuffleParameters() {
-        this.sketch.clear();
-        this.sketch.CONFIG.ATTRACTOR.a = Math.random() * 4 - 2;
-        this.sketch.CONFIG.ATTRACTOR.b = Math.random() * 4 - 2;
-        this.sketch.CONFIG.ATTRACTOR.c = Math.random() * 4 - 2;
-        this.sketch.CONFIG.ATTRACTOR.d = Math.random() * 4 - 2;
-        this.sketch.plotField();
-    }
 
-    run() {
+    toggleRun() {
         this.sketch.running = !this.sketch.running;
     }
 
-    renderField() {
-        this.sketch.plotField();
-    }
-
-    renderForce() {
-        this.sketch.steppedRender();
+    plotField() {
+        this.sketch.render();
+        this.sketch.update();
     }
 
 
     reset() {
         this.sketch.clear();
         this.sketch.running = false;
-
-        this.sketch.forceStep = 0;
-
-        for (var i = 0; i < this.sketch.points.length; i++) {
-            let p = this.sketch.points[i];
-            p.x = 0;
-            p.y = i * 10;
-            p.vx = 0;
-            p.vy = 0;
-        }
     }
 
 
